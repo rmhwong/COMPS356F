754eb93480a934a362168178b66e583bdb0d3cc6
rmhwong <rmhwong@ouhk.edu.hk>
2016-02-24 11:26:51 +0800

    Merge origin/master
    Conflicts:
    	COMPS356F/src/comps356f/COMPS356F.java

---
+++ COMPS356F/src/comps356f/COMPS356F.java
@@ -0,0 +1,21 @@
+/*
+ * By: Raymond Wong
+ */
+package comps356f;
+
+/**
+ *
+ * @author rmhwong
+ */
+public class COMPS356F {
+
+     /**
+      * @param args the command line arguments
+      */
+     public static void main(String[] args) {
+          System.out.println("Hello World");
+          System.out.println("Welcome to GitHub");
+          // TODO code application logic here
+     }
+     
+}
