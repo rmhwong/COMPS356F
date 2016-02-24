325fb55dd33b3c6bedecb4ba1b349c3c59206c3d
Raymond Wong <rmhwong@ouhk.edu.hk>
2016-02-24 13:49:01 +0800

    Update COMPS356F.java

--- COMPS356F.java
+++ COMPS356F.java
@@ -20,6 +20,7 @@ public class COMPS356F {
           System.out.println("Github Sample");
           System.out.println("Github Sample1");
           System.out.println("Github Sample2");
+          System.out.println("Github Sample3");
      }
      
 }
